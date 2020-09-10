899016ad5bb03469d2c0388bd6d6253c83a7d92f
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-09-10 10:15:33 -0400

    feat: adiciona paginação na rota de series

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -8,9 +8,9 @@ class BaseController
 {
     protected $classe;
 
-    public function index()
+    public function index(Request $request)
     {
-        return $this->classe::all();
+        return $this->classe::paginate($request->per_page);
     }
 
     public function store(Request $request)
