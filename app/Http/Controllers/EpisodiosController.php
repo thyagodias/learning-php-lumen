10f5fd818761d9af139da633da1c07f37e899c32
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-09-25 08:54:51 -0400

    feat: adiciona busca por séries

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -10,4 +10,10 @@ class EpisodiosController extends BaseController
     {
         $this->classe = Episodio::class;
     }
+
+    public function buscaPorSerie(int $serieId)
+    {
+
+        return Episodio::query()->where('serie_id', $serieId)->get();
+    }
 }
